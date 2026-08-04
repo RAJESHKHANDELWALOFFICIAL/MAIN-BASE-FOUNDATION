8f0a78eeb0ab0d54a29992f2ab4c4a71519afac1
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-04 15:04:44 +0530

    Update connection.py

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,7 +1,11 @@
+import sqlite3
+
+
 class DatabaseConnection:
 
+    def __init__(self):
+        self.database_name = "main_base_foundation.db"
+
     def connect(self):
-        return {
-            "database": "MAIN BASE FOUNDATION",
-            "status": "Connected"
-        }
+        connection = sqlite3.connect(self.database_name)
+        return connection
