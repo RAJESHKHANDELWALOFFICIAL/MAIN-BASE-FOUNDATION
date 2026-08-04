47b3049ffe8dcfaba1d1aabd3b782486d76f33cf
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-04 09:24:27 +0530

    Add ConfigService class for application configuration

---
+++ backend/config/service.py
@@ -0,0 +1,10 @@
+class ConfigService:
+
+    def get_app_name(self):
+        return "MAIN BASE FOUNDATION"
+
+    def get_version(self):
+        return "1.0"
+
+    def get_status(self):
+        return "Development"
