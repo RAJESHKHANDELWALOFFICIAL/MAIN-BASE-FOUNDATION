94a457840cf5da719ecebf873b69556120af20ff
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-05 17:09:49 +0530

    Update model.py

--- backend/permissions/model.py
+++ backend/permissions/model.py
@@ -1,1 +1,12 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Permission:
+
+    permission_id: str
+    permission_name: str
+    display_name: str
+    description: str
+
+    status: str = "ACTIVE"
