430d649c904f3821f9f5ba7a500bdaf5a857edc2
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-05 15:26:27 +0530

    Add User dataclass with user attributes

--- backend/users/model.py
+++ backend/users/model.py
@@ -1,1 +1,13 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class User:
+    user_id: str
+    full_name: str
+    username: str
+    email: str
+    phone: str
+    password: str
+    role: str = "USER"
+    status: str = "ACTIVE"
