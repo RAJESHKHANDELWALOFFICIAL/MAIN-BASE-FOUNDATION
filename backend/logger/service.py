d837c9ea9d63cd5b9eaace3479b7f3ef0b9fea9f
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-05 08:17:52 +0530

    Implement LoggerService with logging methods

--- backend/logger/service.py
+++ backend/logger/service.py
@@ -1,1 +1,19 @@
+from backend.logger.connection import LoggerConnection
 
+
+class LoggerService:
+
+    def __init__(self):
+        self.logger = LoggerConnection().get_logger()
+
+    def info(self, module, message):
+        self.logger.info(f"[{module}] {message}")
+
+    def warning(self, module, message):
+        self.logger.warning(f"[{module}] {message}")
+
+    def error(self, module, message):
+        self.logger.error(f"[{module}] {message}")
+
+    def critical(self, module, message):
+        self.logger.critical(f"[{module}] {message}")
