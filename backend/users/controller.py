0904d55798118432ec7029607a2b3d35c7b14e56
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-05 15:32:21 +0530

    Update controller.py

--- backend/users/controller.py
+++ backend/users/controller.py
@@ -1,1 +1,30 @@
+from backend.users.service import UserService
 
+
+class UserController:
+
+    def __init__(self):
+        self.service = UserService()
+
+    def initialize(self):
+        return self.service.initialize()
+
+    def register(self, **kwargs):
+        user = self.service.create_user(**kwargs)
+        self.service.save_user(user)
+        return user
+
+    def get(self, user_id):
+        return self.service.get_user(user_id)
+
+    def update(self, **kwargs):
+        self.service.update_user(**kwargs)
+
+    def delete(self, user_id):
+        self.service.delete_user(user_id)
+
+    def list(self):
+        return self.service.get_all_users()
+
+    def search(self, username):
+        return self.service.search_user_by_username(username)
