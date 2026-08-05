736c9746920e47573c56bfcf32dab127ff8cd5b7
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-05 08:26:40 +0530

    Initialize services in core module

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,1 +1,7 @@
+def __init__(self):
+    self.logger = LoggerService()
 
+    self.config = ConfigService()
+    self.database = DatabaseService()
+    self.identity = IdentityService()
+    self.authentication = AuthenticationService()
