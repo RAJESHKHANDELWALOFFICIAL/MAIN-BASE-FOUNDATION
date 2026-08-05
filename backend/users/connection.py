45bf6f80af99055c64ca1ba17291b5ea95852d96
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-05 15:29:26 +0530

    Add UserConnection class for database connection

--- backend/users/connection.py
+++ backend/users/connection.py
@@ -1,1 +1,10 @@
+from backend.database.connection import DatabaseConnection
 
+
+class UserConnection:
+
+    def __init__(self):
+        self.database = DatabaseConnection().connect()
+
+    def connect(self):
+        return self.database
