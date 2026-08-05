e4eb3718d69758e6b288320547b776f73feb3fcd
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-05 15:57:07 +0530

    Add OrganizationController to manage organizations

--- backend/organizations/controller.py
+++ backend/organizations/controller.py
@@ -1,1 +1,13 @@
+from backend.organizations.service import OrganizationService
 
+
+class OrganizationController:
+
+    def __init__(self):
+        self.service = OrganizationService()
+
+    def initialize(self):
+        return self.service.initialize()
+
+    def create(self, **kwargs):
+        return self.service.create_organization(**kwargs)
