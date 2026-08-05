ec1f121b0bc94bb36be6194082e2857c9ee0d546
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-05 17:22:36 +0530

    Update model.py

--- backend/teams/model.py
+++ backend/teams/model.py
@@ -1,1 +1,14 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Team:
+
+    team_id: str
+    team_name: str
+    display_name: str
+    organization_id: str
+    team_leader_id: str
+    description: str
+
+    status: str = "ACTIVE"
