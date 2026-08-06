a518783a16f071097d6d1249c7059419c431aa93
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-06 20:37:01 +0530

    Update generator.py

--- backend/identity/generator.py
+++ backend/identity/generator.py
@@ -4,56 +4,69 @@ import string
 
 class IdentityGenerator:
 
+    LENGTH = 6
+
+    @staticmethod
+    def _generate(prefix: str):
+        return prefix + "-" + "".join(
+            random.choices(
+                string.ascii_uppercase + string.digits,
+                k=IdentityGenerator.LENGTH
+            )
+        )
+
     @staticmethod
     def generate_master_id():
-        return "MBF-" + "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=6)
-        )
+        return IdentityGenerator._generate("MBF")
 
     @staticmethod
     def generate_identity_id():
-        return "IDT-" + "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=6)
-        )
+        return IdentityGenerator._generate("IDT")
+
+    @staticmethod
+    def generate_supreme_id():
+        return IdentityGenerator._generate("SUP")
 
     @staticmethod
     def generate_user_id():
-        return "USR-" + "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=6)
-        )
-
-    @staticmethod
-    def generate_organization_id():
-        return "ORG-" + "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=6)
-        )
-
-    @staticmethod
-    def generate_business_id():
-        return "BUS-" + "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=6)
-        )
-
-    @staticmethod
-    def generate_team_id():
-        return "TEM-" + "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=6)
-        )
-
-    @staticmethod
-    def generate_project_id():
-        return "PRJ-" + "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=6)
-        )
+        return IdentityGenerator._generate("USR")
 
     @staticmethod
     def generate_profile_id():
-        return "PRO-" + "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=6)
-        )
+        return IdentityGenerator._generate("PRO")
+
+    @staticmethod
+    def generate_team_id():
+        return IdentityGenerator._generate("TEM")
+
+    @staticmethod
+    def generate_organization_id():
+        return IdentityGenerator._generate("ORG")
+
+    @staticmethod
+    def generate_business_id():
+        return IdentityGenerator._generate("BUS")
+
+    @staticmethod
+    def generate_project_id():
+        return IdentityGenerator._generate("PRJ")
+
+    @staticmethod
+    def generate_role_id():
+        return IdentityGenerator._generate("ROL")
+
+    @staticmethod
+    def generate_permission_id():
+        return IdentityGenerator._generate("PER")
 
     @staticmethod
     def generate_session_id():
-        return "SES-" + "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=6)
-        )
+        return IdentityGenerator._generate("SES")
+
+    @staticmethod
+    def generate_api_key():
+        return IdentityGenerator._generate("API")
+
+    @staticmethod
+    def generate_token():
+        return IdentityGenerator._generate("TOK")
