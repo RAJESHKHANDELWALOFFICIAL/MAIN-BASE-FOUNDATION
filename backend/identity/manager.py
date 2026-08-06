73e630de3e7ca0a8b037b2e355334875380ef1ba
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-06 21:51:25 +0530

    Add methods to IdentityManager for controller interaction

--- backend/identity/manager.py
+++ backend/identity/manager.py
@@ -6,3 +6,15 @@ class IdentityManager:
     def __init__(self):
 
         self.controller = IdentityController()
+
+    def initialize(self):
+        return self.controller.initialize()
+
+    def create(self, **kwargs):
+        return self.controller.create(**kwargs)
+
+    def get(self, master_id):
+        return self.controller.get(master_id)
+
+    def list(self):
+        return self.controller.list()
