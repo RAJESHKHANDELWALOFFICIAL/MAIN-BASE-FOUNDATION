3f1332283d3560f1e1f91ddb0e660bb530c72f95
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-06 22:05:06 +0530

    Update identity.py

--- backend/api/identity.py
+++ backend/api/identity.py
@@ -9,3 +9,30 @@ router = APIRouter(
 )
 
 controller = IdentityController()
+
+@router.get("/")
+def get_identity(master_id: str):
+    return controller.get(master_id)
+
+
+@router.post("/")
+def create_identity():
+
+    identity = controller.create(
+        supreme_id="SUP-000001",
+        full_name="DR RAJESH KHANDELWAL IBC",
+        display_name="DR RAJESH KHANDELWAL IBC",
+        username="RAJESHKHANDELWALOFFICIAL",
+        email="demo@example.com",
+        phone="910000000000"
+    )
+
+    return {
+        "message": "Identity Created Successfully",
+        "data": identity
+    }
+
+
+@router.get("/list")
+def list_identity():
+    return controller.list()
