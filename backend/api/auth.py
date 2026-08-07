51fc30b991f70648894457b7f417fb4c27514aee
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-07 08:04:25 +0530

    Implement authentication routes in auth.py

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,1 +1,35 @@
+from fastapi import APIRouter
 
+from backend.auth.controller import AuthenticationController
+
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+controller = AuthenticationController()
+
+
+@router.get("/initialize")
+def initialize():
+
+    return controller.initialize()
+
+
+@router.post("/login")
+def login(master_id: str):
+
+    return controller.login(master_id)
+
+
+@router.post("/authenticate")
+def authenticate(master_id: str):
+
+    return controller.authenticate(master_id)
+
+
+@router.post("/logout")
+def logout():
+
+    return controller.logout()
