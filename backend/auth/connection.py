ac955344ff55ec0a44f1faf844903290cea697a9
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-08-07 07:40:13 +0530

    Update connection.py

--- backend/auth/connection.py
+++ backend/auth/connection.py
@@ -4,7 +4,31 @@ from backend.database.connection import DatabaseConnection
 class AuthenticationConnection:
 
     def __init__(self):
-        self.database = DatabaseConnection().connect()
+
+        self.connection = DatabaseConnection().connect()
 
     def connect(self):
-        return self.database
+
+        return self.connection
+
+    def cursor(self):
+
+        return self.connection.cursor()
+
+    def commit(self):
+
+        self.connection.commit()
+
+    def rollback(self):
+
+        self.connection.rollback()
+
+    def close(self):
+
+        self.connection.close()
+
+    def health(self):
+
+        return {
+            "authentication": "CONNECTED"
+        }
