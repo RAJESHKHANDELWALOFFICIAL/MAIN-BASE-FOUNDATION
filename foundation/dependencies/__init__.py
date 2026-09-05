868d3527aa322ddbfc3c5019074a2df222b396c5
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-09-05 11:39:24 +0530

    Add docstring and import statements to __init__.py

--- foundation/dependencies/__init__.py
+++ foundation/dependencies/__init__.py
@@ -1,1 +1,16 @@
+"""
+MAIN BASE FOUNDATION
+Dependency Package
+"""
 
+from foundation.dependencies.index import (
+    Dependency,
+    DependencyIndex,
+    dependency_index,
+)
+
+__all__ = [
+    "Dependency",
+    "DependencyIndex",
+    "dependency_index",
+]
