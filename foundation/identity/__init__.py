0118c2f14c86d24c3a98b959747cad363dc8ea4a
👑 DR RAJESH KHANDELWAL IBC 👑 <RAJESHKHANDELWAL468@GMAIL.COM>
2026-09-05 09:04:23 +0530

    Add docstring and exports to identity package

--- foundation/identity/__init__.py
+++ foundation/identity/__init__.py
@@ -1,1 +1,16 @@
+"""
+MAIN BASE FOUNDATION
+Identity Package
+"""
 
+from foundation.identity.identity import (
+    Identity,
+    IdentityManager,
+    identity_manager,
+)
+
+__all__ = [
+    "Identity",
+    "IdentityManager",
+    "identity_manager",
+]
